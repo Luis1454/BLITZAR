04243fdc83a1ca87d6b0be1761f3da65da95e42d
Luis FERNANDES <luis.fernandes@epitech.eu>
2026-03-08 14:38:11 -0700

    refactor(issue-23): reorganize check test suites

--- tests/cmake/targets_repo_checks.cmake
+++ tests/cmake/targets_repo_checks.cmake
@@ -48,7 +48,7 @@ add_test(
         --basetemp
         ${CMAKE_BINARY_DIR}/pytest-basetemp-tst-qlt-repo-008
         -q
-        ${GRAVITY_ROOT_DIR}/tests/checks/tests
+        ${GRAVITY_ROOT_DIR}/tests/checks/suites
 )
 set_tests_properties(TST_QLT_REPO_008_PyChecksUnit PROPERTIES LABELS "integration")
 
